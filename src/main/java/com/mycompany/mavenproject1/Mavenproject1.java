6137d3d629a321c40acabea0af95539004535b3b
MirandaTN <115226584+MirandaTN@users.noreply.github.com>
2024-04-29 12:07:27 +0200

    Update Mavenproject1.java

--- src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
+++ src/main/java/com/mycompany/mavenproject1/Mavenproject1.java
@@ -11,7 +11,7 @@ package com.mycompany.mavenproject1;
 public class Mavenproject1 {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.println("Who is back into singlehood?");
     }
 }
  
